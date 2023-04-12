36aad9a46da1d7310498aee5e497c29f721bcabb
pepperonipowered <pepperonipowered@gmail.com>
2023-04-13 02:55:18 +0800

    Initial Commit

---
+++ resources/views/blogs.blade.php
@@ -0,0 +1,7 @@
+<x-pawprint-layout>
+    <x-slot name="nav">
+        @include('layouts.nav')
+    </x-slot> 
+
+    <p class="text-white">Blogs</p>
+</x-pawprint-layout>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+<x-pawprint-layout>
+    <x-slot name="nav">
+        @include('layouts.nav')
+    </x-slot> 
+
+    <p class="text-white">Welcome</p>
+</x-pawprint-layout>
